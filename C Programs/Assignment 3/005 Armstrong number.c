5caf70c52e90b6d47f209ea84ac0c0c6e6f88836
Omkar7118 <omkarmankar7118@gmail.com>
2025-07-03 22:47:18 +0530

    Add files via upload

---
+++ C Programs/Assignment 3/005 Armstrong number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+	int number,original_number,result=0,reminder;
+	printf("Enter a number : ");
+	scanf("%d",&number);
+	original_number=number;
+	while(number!=0)
+	{
+		reminder=number%10;
+		result=reminder*reminder*reminder+result;
+		number=number/10;
+	}
+	if(result==original_number)
+	{
+		printf("Number is Armstrong");
+	}
+	else
+	{
+		printf("Number is not Armstrong");
+	}
+}
